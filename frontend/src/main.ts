a834402822f5187acd3a54924683f21ae7e3a6d0
AlexNT-maker <alexnter98@gmail.com>
2026-08-01 15:59:45 +0300

    Extract payroll handlers

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -15,10 +15,13 @@ import {
     initExpensesEvents,
     initExpensesPage,
 } from "./handlers/expenseEvents"
+import { initPayrollEvents } from "./handlers/payrollEvents";
 
 const datePicker = document.querySelector<HTMLInputElement>('#date-picker')!;
 const btnEditDaily = document.querySelector<HTMLButtonElement>('#btn-edit-daily')!;
 const sortSelect = document.getElementById('sort-select') as HTMLSelectElement | null;
+const payStart = document.querySelector<HTMLInputElement>('#pay-start')!;
+const payEnd = document.querySelector<HTMLInputElement>('#pay-end')!;
 
 // Set as default today
 datePicker.valueAsDate = new Date();
@@ -73,9 +76,7 @@ navButtons.shortAnalysis.addEventListener('click', () => navigateTo('shortAnalys
 
 // 7. -- Employee management logic -- 
 
-const modalExtra = document.querySelector<HTMLDivElement>('#modal-extra')!;
-const extraForm = document.querySelector<HTMLFormElement>('#extra-form')!;
-const btnCancelExtra = document.querySelector<HTMLButtonElement>('#btn-cancel-extra')!;
+
 
 renderEmployeesList();
 
@@ -86,16 +87,9 @@ initBoatEvents();
 initBoatAnalysisEvents();
 
 initExpensesEvents();
-// Calculate wage
-// -- 15. Payments Logic/ Payroll --
 
-const payStart = document.querySelector<HTMLInputElement>('#pay-start')!;
-const payEnd = document.querySelector<HTMLInputElement>('#pay-end')!;
-const btnCalcPayroll = document.querySelector<HTMLButtonElement>('#btn-calc-payroll')!;
-const btnPrintPayroll = document.querySelector<HTMLButtonElement>('#btn-print-payroll')!;
-const payrollListBody = document.querySelector<HTMLTableSectionElement>('#payroll-list')!;
-const payrollActions = document.querySelector<HTMLDivElement>('#payroll-actions')!;
-const btnClearExtra = document.querySelector<HTMLButtonElement>('#btn-clear-extra')!; 
+initPayrollEvents();
+
 
 function initPayrollPage(){
   if (!payStart.value){
@@ -107,177 +101,6 @@ function initPayrollPage(){
   }
 }
 
-// Calculate wage & Render Table
-btnCalcPayroll.addEventListener('click', async () => {
-    const start = payStart.value;
-    const end = payEnd.value;
-
-    if (!start || !end) {
-        alert("Παρακαλώ επιλέξτε ημερομηνίες.");
-        return;
-    }
-
-    try {
-        const response = await fetch(`http://127.0.0.1:8000/payroll/?start=${start}&end=${end}`);
-        
-        if (response.ok) {
-            const data = await response.json(); 
-
-            payrollListBody.innerHTML = '';
-            
-            if (data.payments.length === 0) {
-                payrollListBody.innerHTML = '<tr><td colspan="8" style="text-align:center;">Δεν βρέθηκαν πληρωμές για αυτό το διάστημα.</td></tr>';
-                payrollActions.classList.add('hidden');
-                return;
-            }
-            payrollActions.classList.remove('hidden');
-
-            data.payments.forEach((item: any) => {
-                const row = document.createElement('tr');
-                
-                row.innerHTML = `
-                    <td style="font-weight: 500;">${item.employee_name}</td>
-                    <td style="text-align: center;">${item.days_worked}</td>
-                    <td>${item.total_wage.toFixed(2)} €</td>
-                    <td>${item.total_overtime.toFixed(2)} €</td>
-                    <td style="font-weight: 800;">${item.grand_total.toFixed(2)} €</td>
-                    
-                    <td style="background-color: #eff6ff; color: #1e40af; font-weight: bold;">
-                        ${item.bank_pay.toFixed(2)} €
-                    </td>
-                    
-                    <td style="background-color: #fffbeb; color: #92400e; font-weight: bold;">
-                        ${item.cash_pay.toFixed(2)} €
-                    </td>
-                    
-                    <td>
-                        <button class="action-btn btn-add-extra" data-id="${item.employee_id}" 
-                                style="background-color: #dd780b; color: white; cursor: pointer;">
-                            Πρόσθετα
-                        </button>
-                    </td>
-                `;
-                payrollListBody.appendChild(row);
-            });
-
-        } else {
-            alert("Σφάλμα κατά τη λήψη μισθοδοσίας.");
-        }
-    } catch (error) {
-        console.error(error);
-        alert("Σφάλμα σύνδεσης.");
-    }
-});
-
-
-// -- Extra features logic ---
-
-// 1. Modal Open
-payrollListBody.addEventListener('click', (e) => {
-    const target = e.target as HTMLElement;
-    if (target.classList.contains('btn-add-extra')) {
-        const empId = target.dataset.id;
-        (document.getElementById('extra-emp-id') as HTMLInputElement).value = empId!;
-        (document.getElementById('extra-amount') as HTMLInputElement).value = '';
-        (document.getElementById('extra-reason') as HTMLInputElement).value = '';
-        
-        if(modalExtra) modalExtra.classList.remove('hidden');
-    }
-});
-
-// 2. Close Modal
-if(btnCancelExtra) {
-    btnCancelExtra.addEventListener('click', () => {
-        if(modalExtra) modalExtra.classList.add('hidden');
-    });
-}
-
-// 3. Submit Logic
-if(extraForm) {
-    extraForm.addEventListener('submit', async (e) => {
-        e.preventDefault();
-        const empId = (document.getElementById('extra-emp-id') as HTMLInputElement).value;
-        const amount = parseFloat((document.getElementById('extra-amount') as HTMLInputElement).value);
-        const reason = (document.getElementById('extra-reason') as HTMLInputElement).value;
-
-        const payload = {
-            date: payEnd.value,
-            employee_id: parseInt(empId),
-            boat_id: null,      
-            present: null,      
-            overtime_hours: null,
-            extra_amount: amount,
-            extra_reason: reason
-        };
-
-        try {
-            const res = await fetch('http://127.0.0.1:8000/attendance/', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(payload)
-            });
-
-            if (res.ok) {
-                if(modalExtra) modalExtra.classList.add('hidden');
-                btnCalcPayroll.click(); 
-                alert("Το Extra αποθηκεύτηκε!");
-            }
-        } catch (err) {
-            console.error(err);
-            alert("Σφάλμα σύνδεσης");
-        }
-    });
-}
-
-// 4. Reset Logic
-if (btnClearExtra) {
-    btnClearExtra.addEventListener('click', async () => {
-        const empId = (document.getElementById('extra-emp-id') as HTMLInputElement).value;
-        
-        const payload = {
-            date: payEnd.value,
-            employee_id: parseInt(empId),
-            boat_id: null,
-            present: null,
-            overtime_hours: null,
-            extra_amount: 0,    
-            extra_reason: ""    
-        };
-
-        if (confirm("Θέλετε να διαγράψετε το Extra ποσό;")) {
-            try {
-                const res = await fetch('http://127.0.0.1:8000/attendance/', {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify(payload)
-                });
-
-                if (res.ok) {
-                    if(modalExtra) modalExtra.classList.add('hidden');
-                    btnCalcPayroll.click(); 
-                    alert("Το Extra διαγράφηκε!");
-                }
-            } catch (err) {
-                console.error(err);
-            }
-        }
-    });
-}
-
-// PDF Logic
-if(btnPrintPayroll) {
-    btnPrintPayroll.addEventListener('click', () => {
-        const start = payStart.value;
-        const end = payEnd.value;
-
-        if (!start || !end) {
-            alert("Παρακαλώ επιλέξτε ημερομηνίες.");
-            return;
-        }
-        const url = `http://127.0.0.1:8000/payroll/pdf?start=${start}&end=${end}`;    
-        window.open(url, '_blank');
-    });
-}
 
 // --- SHORT ANALYSIS LOGIC ---
 const shortBoatSelect = document.querySelector<HTMLSelectElement>('#short-boat-select')!;
